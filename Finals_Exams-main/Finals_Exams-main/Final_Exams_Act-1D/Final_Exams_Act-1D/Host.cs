4d32deca994bf389ccf03070088017034a262a85
Lady Nicole P Cielo <valriescarlet@gmail.com>
2026-04-29 14:32:58 +0800

    Add files via upload

---
+++ Finals_Exams-main/Finals_Exams-main/Final_Exams_Act-1D/Final_Exams_Act-1D/Host.cs
@@ -0,0 +1,61 @@
+﻿using Final_Exam_Act_1D;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Final_Exams_Act_1D
+{
+    public partial class Host : Form
+    {
+        public Host()
+        {
+            InitializeComponent();
+        }
+
+        private void txtModel_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSendHost_Click(object sender, EventArgs e)
+        {
+            string firstname = txtFirstName.Text.Trim();
+            string lastname = txtLastName.Text.Trim();
+            string contactnum = txtContactNum.Text.Trim();
+            string email = txtEmail.Text.Trim();
+            string vehicletype = cmbTypeVehicle.SelectedIndex.ToString();
+            string model = txtModel.Text.Trim();
+            string platenum = txtPlateNum.Text.Trim();
+            string note = txtNote.Text.Trim();
+
+            if (string.IsNullOrEmpty(firstname) || string.IsNullOrEmpty(lastname) || string.IsNullOrEmpty(contactnum) || string.IsNullOrEmpty(email) ||
+                string.IsNullOrEmpty(vehicletype) || string.IsNullOrEmpty(model) || string.IsNullOrEmpty(platenum) || string.IsNullOrEmpty(note))
+            {
+                MessageBox.Show("Please fill all Fields!");
+                return;
+            }
+
+            DBConnect db = new DBConnect();
+
+            try
+            {
+                db.Open();
+
+                string query = "INSERT INTO owner_approval (name, contact, vehicle_type, vehicle_model, plate_number)" + "VALUES (@firstname, @contactnum, @vehicletype, @model, @platenum)";
+
+                MySql.Data.MySqlClient.MySqlCommand cmd =
+                    new MySql.Data.MySqlClient.MySqlCommand(query, db.Connection);
+            } 
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + ex.Message);
+            }
+
+            finally { db.Close(); }
+        }
+    }
+}
---
+++ Finals_Exams-main/Finals_Exams-main/Final_Exams_Act-1D/Final_Exams_Act-1D/UserDashboard.cs
@@ -0,0 +1,131 @@
+﻿using Final_Exam_Act_1D;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Final_Exams_Act_1D
+{
+    public partial class UserDashboard : Form
+    {
+        public UserDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            Login login = new Login();
+            login.Show();
+            this.Hide();
+        }
+
+        private void btnHost_Click(object sender, EventArgs e)
+        {
+            Host host = new Host();
+            host.Show();
+            this.Hide();
+        }
+
+        private void btnRequest_Click(object sender, EventArgs e)
+        {
+            Request request = new Request();
+            request.Show();
+            this.Hide();
+        }
+
+        private void UserDashboard_Load(object sender, EventArgs e)
+        {
+            LoadVehicle();
+        }
+
+        private void LoadVehicle()
+        {
+            string search = txtSearch.Text.Trim();
+
+
+
+            DBConnect db = new DBConnect();
+
+            try
+
+            {
+
+                db.Open();
+
+                string query = @"SELECT vehicle_id, Name, contact, User_Id, Vehicle_type, Vehicle_model, Plate_number, availability FROM vehicle WHERE Name LIKE @search OR contact LIKE @search OR User_Id LIKE @search OR Vehicle_type LIKE @search OR Vehicle_model LIKE @search OR Plate_Number LIKE @search OR availability LIKE @availability";
+
+
+
+                MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(query, db.Connection);
+
+                cmd.Parameters.AddWithValue("@search", "%" + search + "%");
+
+
+
+                MySql.Data.MySqlClient.MySqlDataAdapter adapter = new MySql.Data.MySqlClient.MySqlDataAdapter(cmd);
+
+                System.Data.DataTable table = new System.Data.DataTable();
+
+                adapter.Fill(table);
+
+                dgvVehicle.DataSource = table;
+
+
+
+                dgvVehicle.Columns["vehicle_id"].HeaderText = "VEHICLE ID";
+
+                dgvVehicle.Columns["namw"].HeaderText = "Name";
+
+                dgvVehicle.Columns["contact"].HeaderText = "Contact";
+
+                dgvVehicle.Columns["user_id"].HeaderText = "User ID";
+
+                dgvVehicle.Columns["vehicle_type"].HeaderText = "Vehicle Type";
+
+                dgvVehicle.Columns["vehicle_Model"].HeaderText = "Vehicle Model";
+
+                dgvVehicle.Columns["plate_number"].HeaderText = "Plate Number";
+
+                dgvVehicle.Columns["availability"].HeaderText = "Availability";
+
+
+
+                adapter.Dispose();
+
+                cmd.Dispose();
+
+            }
+
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+            finally
+            {
+
+                db.Close();
+
+            }
+
+        }
+
+
+        private void dgvVehicle_CellClick(object sender, DataGridViewCellEventArgs e)
+
+        {
+
+        }
+
+        private void dgvVehicle_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+
+    }
+}
